794b497f4da12951b8826c4c271ee011f75e2bb8
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-18 16:50:51 +0300

    add main file

--- main.c
+++ main.c
@@ -16,6 +16,7 @@ int main(void)
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
+
 	ui = (unsigned int) INT_MAX + 1024;
 	addr = (void *) 0x7ffe637541f0;
 	_printf("Length:[%d, %i]\n", len, len);
