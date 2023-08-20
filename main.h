33a58c21e478c1cb33b91062802b165d5fc3bc5d
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-08-20 03:38:33 -0700

    My Headers.

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+/*Main Fnction*/
+int _printf(const char *format, ...);
+int _putchar(char c);
+int check(char ch,va_list arg);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_per(va_list arg);
+int print_int(va_list arg);
+int print_binary(va_list arg);
+int print_unsigned_integer(va_list arg);
+int get_size(unsigned int n, int b);
+
+
+
+
+
+
+#endif
