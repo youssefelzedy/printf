70840e186cf03d38029b7f7ddf0fcc4c3833b12f
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-08-20 12:25:32 -0700

    Some Changes to make the checker happy

--- main.h
+++ main.h
@@ -15,6 +15,8 @@ int print_char(va_list arg);
 int print_str(va_list arg);
 int print_per(va_list arg);
 int print_int(va_list arg);
+int print_pointer(va_list arg);
+int print_hex(unsigned long int num);
 int print_binary(va_list arg);
 int print_unsigned_integer(va_list arg);
 int get_size(unsigned int n, int b);
