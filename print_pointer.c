6a4950334ca2bc1ad787d134c8bbd833c78139b2
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-08-20 12:46:15 -0700

    Changes to make the checker happy

---
+++ print_pointer.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * printf_pointer - prints an hexgecimal number.
+ * @arg: arguments.
+ * Return: counter.
+ */
+int print_pointer(va_list arg)
+{
+	void *p;
+	char *s = "(nil)";
+	long int a;
+	int x;
+	int i;
+
+	p = va_arg(arg, void*);
+	if (p == NULL)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			_putchar(s[i]);
+		}
+		return (i);
+	}
+
+	a = (unsigned long int)p;
+	_putchar('0');
+	_putchar('x');
+	x = printf_hex_aux(a);
+	return (x + 2);
+}
