5dbf1d66daa7a5c30695326e9713c0a0e9939daf
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-20 13:41:43 +0300

    Func To print integer

---
+++ print_int.c
@@ -0,0 +1,71 @@
+
+#include "main.h"
+
+/**
+ * print_int - to print print unsigned integer
+ * @arg: num should be print
+ * Return: A total count of the characters printed.
+ */
+
+int print_int(va_list arg) {
+
+    int number = va_arg(arg, int), div, len;
+    unsigned int num;
+
+    div = 1;
+    len = 0;
+
+    if (number < 0)
+    {
+        len += _putchar('-');
+        num = -number;
+    }
+    else
+        num = number;
+
+    while (num / div > 9)
+        div *= 10;
+
+    while (div != 0)
+    {
+        len += _putchar('0' + (num / div));
+        num %= div;
+        div /= 10;
+    }
+    return (len);
+}
+
+/**
+ * print_unsigned_integer - to print print unsigned integer
+ * @arg: num should be print
+ * Return: A total count of the characters printed.
+ */
+
+int print_unsigned_integer(va_list arg)
+{
+    int div, len;
+    unsigned int num = va_arg(arg, unsigned int);
+
+    if(num == 0)
+    {
+        _putchar('0');
+        return(1);
+    }
+
+    div = 1;
+    len = 0;
+
+    while (num / div > 9)
+    {
+        div *= 10;
+    }
+
+    while (div != 0)
+    {
+        len += _putchar('0' + (num / div));
+        num %= div;
+        div /= 10;
+    }
+    return (len);
+}
+
