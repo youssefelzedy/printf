a99b14247300de435213a9f5f4b35e07c7a96449
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-08-21 03:01:03 -0700

    Changes to make the checker happy

--- print_char.c
+++ print_char.c
@@ -16,33 +16,6 @@ int print_char(va_list arg)
 }
 
 /**
- *print_str - to print string charactar
- *@arg: taking v_list
- *Return: A total count of the characters printed.
- */
-
-int print_str(va_list arg)
-{
-	char *str;
-	int i = 0, length;
-
-	str = va_arg(arg, char *);
-	if (str == NULL)
-	{
-		str = "(null)";
-	}
-
-	length = _strlen(str);
-	while (i < length)
-	{
-		_putchar(str[i]);
-		i++;
-	}
-
-	return (length);
-}
-
-/**
  *print_per - to print % charactar
  *@arg: taking v_list
  *Return: A total count of the characters printed.
